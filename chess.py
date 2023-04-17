626721c144ef77f39ad3174862cd22ba9e6e8aec
Rohan Chugh <rohanchugh14@gmail.com>
2023-04-17 15:42:49 -0500

    added chess docs

--- chess.py
+++ chess.py
@@ -4,6 +4,15 @@ from board import Board
 
 
 class Chess:
+   """
+   A class to represent a game of chess.
+   This class focuses on handling aspects specific to the game, i.e. castling
+   rights, whose turn it is, whether a player is in check, etc.
+
+   It also parses player moves and validates whether or not they are legal,
+   and then sends the valid moves to the Board class to update the board using
+   simpler notation. 
+   """
 
 
    def __init__(self, fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'):
